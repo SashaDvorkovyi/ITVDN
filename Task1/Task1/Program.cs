ef6519ef7d303fad0b9547a5a816ffd06d40f5f5
DVORKOVIJ-ALEX\sdo <sasha3489@ukr.net>
2018-03-16 13:20:06 +0300

    no message

---
+++ Task1/Task1/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var text =string.Empty;
+            try
+            {
+                using (StreamReader strem = new StreamReader("D:/INPUT.txt"))
+                {
+                    text += strem.ReadToEnd();
+                    if (text.Length > 100)
+                    {
+                        text = text.Substring(0, 99);
+                    }
+
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("The process failed: {0}", e.ToString());
+            }
+            var theLongest = string.Empty;
+            var theExisting = string.Empty;
+            try
+            {
+                for (var i = 0; i <= text.Length; i++)
+                {
+                    if (i != text.Length && (int.Parse(text[i].ToString()) != 0))
+                    {
+                        theExisting += text[i];
+                    }
+                    else
+                    {
+                        if (theLongest != null && theExisting != null)
+                        {
+                            theLongest = theLongest.Length < theExisting.Length ? theExisting : theLongest;
+                            theExisting = string.Empty;
+                        }
+                        else if (theLongest == null && theExisting != null)
+                        {
+                            theLongest = theExisting;
+                            theExisting = string.Empty;
+                        }
+                    }
+                }
+            }
+            catch
+            {
+                Console.WriteLine("sorry but in the file 'INPUT.txt' must be only 0 and 1");
+            }
+            
+            var path = "D:/OUTPUT.txt";
+            if (File.Exists(path))
+            {
+                File.Delete(path);
+            }
+            using (StreamWriter sw = new StreamWriter(path))
+            {
+                sw.WriteLine(theLongest);
+            }
+
+        }
+    }
+}
